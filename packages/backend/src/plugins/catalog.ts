a6cfefeff930f0f03ec5bf79fa5fd5572b715863
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2023-08-07 21:35:47 -0300

    configure backstage in org mode
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,25 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import { GithubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'development',
+      orgUrl: 'https://github.com/Teste-oauth-app',
+      logger: env.logger,
+      // optional: alternatively, use scheduler with schedule defined in app-config.yaml
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 30 },
+        timeout: { minutes: 3 },
+      }),
+    }
+    ));
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
